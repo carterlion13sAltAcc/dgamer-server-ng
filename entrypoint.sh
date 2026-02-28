a58e9a63403cd7f52636e33bfb561ea76e8488d1
carterlion13sAltAcc <carterlion1513@gmail.com>
2026-02-28 16:47:20 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,58 @@
-#!/bin/bash
+FROM debian:bullseye-slim
+LABEL name="nintendo-dgamer-pi"
+LABEL description="Optimized DGamer server for Raspberry Pi (ARM)"
 
-# Replace the "HOST_IP" with the ip provided in the environment
+# Install build dependencies + dnsmasq (replaces bind9 for better Pi performance)
+RUN apt-get update && apt-get install -y \
+    curl build-essential make libz-dev libbz2-dev \
+    libreadline-dev libexpat1-dev zlib1g-dev libssl-dev \
+    libapr1-dev libaprutil1-dev libpcre3-dev php-cli \
+    php-dev libapache2-mod-php dnsmasq \
+    && rm -rf /var/lib/apt/lists/*
 
-set -e
+# DS Compatibility: Lower OpenSSL security level for legacy handshakes
+RUN sed -i 's/CipherString = DEFAULT@SECLEVEL=2/CipherString = DEFAULT@SECLEVEL=0/' /etc/ssl/openssl.cnf
 
-#if [ "${HOST_IP}" == "" ]; then
-#	echo -e "Error: -e HOST_IP env var is required and must be a valid IPv4 address!"
-#	exit 1
-#fi
+# 1. Compile OpenSSL 1.0.2k (SSLv3 enabled for DS/Wii)
+WORKDIR /tmp
+RUN curl -L https://www.openssl.org | tar -xzf - \
+    && cd openssl-1.0.2k \
+    && ./config --prefix=/usr --openssldir=/usr/lib/ssl -fPIC enable-ssl2 enable-ssl3 no-shared \
+    && make -j$(nproc) \
+    && make install
 
-#echo -e "Hosting on ${HOST_IP} ..."
+# 2. Compile PCRE 8.45
+RUN curl -L https://sourceforge.net | tar -xjf - \
+    && cd pcre-8.45 \
+    && ./configure --prefix=/usr --enable-unicode-properties --enable-pcre16 --enable-pcre32 \
+    && make -j$(nproc) \
+    && make install
 
-# Add FQDNs to hosts file
-echo "127.0.0.1    home.disney.go.com" >> /etc/hosts
+# 3. Compile Apache 2.4.48 with legacy SSL support
+RUN curl -L https://archive.apache.org | tar -xzf - && mv httpd-2.4.48 httpd \
+    && curl -L https://archive.apache.org | tar -xzf - && mv apr-1.7.0 httpd/srclib/apr \
+    && curl -L https://archive.apache.org | tar -xzf - && mv apr-util-1.6.1 httpd/srclib/apr-util \
+    && cd httpd && ./configure \
+    --prefix=/usr/local/apache \
+    --with-included-apr \
+    --enable-ssl \
+    --with-ssl=/usr \
+    --enable-ssl-staticlib-deps \
+    --enable-mods-static=ssl \
+    --enable-mods-shared=all \
+    --enable-so \
+    && make -j$(nproc) \
+    && make install
 
-#sed -i "s/HOST_IP/${HOST_IP}/g" /etc/bind/dgamer.db
+# Setup folders and Copy local files
+RUN mkdir -p /var/www /usr/local/apache/conf /usr/local/apache/certs
+COPY ./sites/ /var/www/
+COPY ./configs/apache/ /usr/local/apache/conf/
+# Uncomment the line below if you have a local certs folder
+# COPY ./certs/ /usr/local/apache/certs/
+COPY ./entrypoint.sh /srv/
+RUN chmod +x /srv/entrypoint.sh
 
-#/etc/init.d/bind9 start
-#/usr/sbin/named -u bind -c /etc/bind/named.conf -g & 
-
-/usr/local/apache/bin/httpd -k stop
-exec /usr/local/apache/bin/httpd -D FOREGROUND
-
+EXPOSE 80 443 53/tcp 53/udp
+WORKDIR /usr/local/apache
+CMD ["/srv/entrypoint.sh"]
